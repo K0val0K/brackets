bdf42c955c5b722c02255222920af92e29226643
Vladislav Kovalenko <kovalok2020@gmail.com>
2021-03-14 18:28:07 +0300

    feat: add completed brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    
+    let configMap = new Map();
+
+    bracketsConfig.forEach(element => {
+        configMap.set(element[0],element[1]);
+    });
+
+    let stack = [];
+    let strArray = str.split('');
+
+    for(let i = 0; i < strArray.length; i++) {
+        if(configMap.has(strArray[i])) {
+            if(strArray[i] == stack[stack.length-1]){
+                stack.pop();
+                continue;
+            }
+            stack.push(configMap.get(strArray[i]));
+        }
+        else {
+            if(strArray[i] == stack[stack.length-1]) {
+                stack.pop()
+            } else {
+                return false;
+            }  
+
+        }
+    };
+    return stack.length == 0;
 }
+ 
